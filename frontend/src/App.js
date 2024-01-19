7df676723efde60810dd3b65cc9d7ea550a56f5d
m4rkbello <markbello10041999@gmail.com>
2024-01-20 02:19:05 +0800

    Install REACT-ROUTER-DOM

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 
 function App() {
   return (
     <div className="App">
- 
-     <Login />
+ {/** 
+ <Login />
+*/}
+<Register />
   
     </div>
   );
