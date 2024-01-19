f2bb3f1ef2e932705f7f5c5869723eba45d22351
m4rkbello <markbello10041999@gmail.com>
2024-01-20 02:27:15 +0800

    Add BrowserRouter, Router, Routes, Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 
 function App() {
   return (
     <div className="App">
- {/** 
- <Login />
-*/}
-<Register />
+    <Router>
+      <Routes>
+        <Route path='/login' exact element={<Login />} />
+        <Route path='/register' exact element={<Register />} />
+      </Routes>
+    </Router>
   
     </div>
   );
